8e515bb36dd918f6a67067697eee46e5378d01b4
Ben <4991309+NuroDev@users.noreply.github.com>
2025-05-10 13:19:06 +0100

    Added placeholder `inspect`, `pull` & `remove` methods to `image` client

--- packages/@nurodev/docker/src/image/mod.ts
+++ packages/@nurodev/docker/src/image/mod.ts
@@ -9,9 +9,32 @@ export class ImageClient extends BaseClient {
 	/**
 	 * @todo Add documentation
 	 */
+	public async inspect(name: string): Promise<unknown> {
+		// TODO(@nurodev): Add `!response.ok` check.
+		const response = await this.get(`/images/${name}/json`);
+		return await response.json();
+	}
+
+	/**
+	 * @todo Add documentation
+	 */
 	public async list(): Promise<ImageList> {
 		// TODO(@nurodev): Add `!response.ok` check.
 		const response = await this.get('/images/json');
 		return await response.json();
+	}
+
+	/**
+	 * @todo Add documentation
+	 */
+	public async pull(): Promise<unknown> {
+		return Promise.resolve([]);
+	}
+
+	/**
+	 * @todo Add documentation
+	 */
+	public async remove(): Promise<unknown> {
+		return Promise.resolve([]);
 	}
 }
