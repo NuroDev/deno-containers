bcb28aaf43a948b064a1fc3b0d9c0c566974441a
Ben <4991309+NuroDev@users.noreply.github.com>
2025-05-10 02:36:19 +0100

    Added initial `list-images` example

--- examples/list-images/main.ts
+++ examples/list-images/main.ts
@@ -1,1 +1,6 @@
-import {} from '@nurodev/containers';
+import { Client } from '@nurodev/containers';
+
+const client = new Client();
+
+const images = await client.images.list();
+console.log(images);
